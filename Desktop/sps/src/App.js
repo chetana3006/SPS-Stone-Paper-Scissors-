04032222dd4093ec89d8a3473a87e72a4e8893b0
Sameerul-Hak <sameerulhakofficial@gmail.com>
2023-12-10 21:05:02 +0530

    gh

--- Desktop/sps/src/App.js
+++ Desktop/sps/src/App.js
@@ -15,22 +15,32 @@ import CreatedRoom from './Screen/TaskAllocation/CreatedRoom';
 function App() {
   const [user, setuser] = useState({ user: '', id: '', isSite: '', isAdmin: '' });
   const [Room, setRoom] = useState({
-    // roomid:"",
+    roomid:"",
     roomCode: '',
     siteEngineerName: '',
-    roomName: '',
+    roomName: '', 
     startDate: '',
     participants: [],
   });
 
   return (
     <div> 
+      <Context.Provider value={{user,setuser}}>
+        <RoomContext.Provider value={{Room,setRoom}}>
       <Router>
         <Routes>
           <Route  path="/"element={<Home/>}/>        
           <Route  path="/labour"element={<Labour/>}/>        
+          <Route  path="/login"element={<Login/>}/>        
+          <Route  path="/register"element={<Register/>}/>        
+          <Route  path="/taskallocation"element={<TaskAllocationcreateroom/>}/>        
+          <Route  path="/complaint"element={<ComplaintView/>}/>        
+          <Route  path="/laboursmessage"element={<LabourMessage/>}/>        
+          <Route  path="/createdroom"element={<CreatedRoom/>}/>        
           </Routes>
       </Router>
+      </RoomContext.Provider>
+      </Context.Provider>
       
     </div>
   );
