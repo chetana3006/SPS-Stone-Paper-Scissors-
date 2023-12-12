00b11e3f83d00ec0a90fb2130b9c3fa5211e098a
Sriram <sriramsivakumar23@gmail.com>
2023-12-12 23:04:55 +0530

    updation

--- Desktop/sps/src/Components/Home/Home.js
+++ Desktop/sps/src/Components/Home/Home.js
@@ -38,35 +38,24 @@ const Home = () => {
   }, []);
   return (
     <div className='flex flex-col h-full'>
-      <div class='flex flex-row justify-between items-center  bg-blue-700 px-5 py-2  '>
-            <div className='flex flex-row    items-center   mb-2 border-gray-200 '>
-            <img src={Profile} alt="profile" className='h-7 w-7 mr-2'/>
-            <h2 class='font-medium poppins text-white text-2xl'>Stone Paper Scissors</h2>
-            </div>
-            
-            <div className='flex flex-row  px-5  items-center   mb-2 border-gray-200 '>
-            <img src={Profile} alt="profile" className='h-7 w-7 mr-2'/>
-            <h2 className='poppins font-medium text-lg text-white'>Karthik</h2>
-
-          </div>
-      </div>
+      
       <div className=' flex flex-row  bglightblue '>
-      <div className='flex flex-col w-80 bglightgrey   py-4 mt-5 ml-4 rounded-lg shadow-md'>
+      <div className='flex flex-col w-80 bglightgrey   py-4  shadow-md'>
         {/* <div className='flex flex-row  px-5  items-center   mb-2 border-gray-200 py-3'>
           <img src={Profile} alt="profile" className='h-7 w-7 mr-2'/>
           <h2 className='poppins font-medium text-lg text-white'>Karthik</h2>
         </div> */}
-        {/* <div className='flex row justify-center items-center w-full   mb-2 border-gray-200 py-3'>
-          {/* <div className='flex flex-col'>
-            <h2 className='poppins text-black font-medium text-2xl'>Stone</h2>
-            <h2 className='poppins text-black font-medium text-2xl'>Paper</h2>
-            <h2 className='poppins text-black font-medium text-2xl'>Scissors</h2>
+        <div className='flex row justify-around items-center w-full   mb-2 border-gray-200 py-3'>
+           <div className='flex flex-col text-left'>
+            <h2 className='poppins lightgreen font-medium text-2xl'>Stone</h2>
+            <h2 className='poppins lightgreen font-medium text-2xl'>Paper</h2>
+            <h2 className='poppins lightgreen font-medium text-2xl'>Scissors</h2>
           </div> 
-          {/* <div className='flex justify-center items-center'>
+           <div className='flex justify-center items-center'>
              <img src={Profile} alt="profile"/>
           </div> 
-        </div> */}
-        <div><h2 className='poppins text-lg ml-4 font-medium'>Search Project</h2></div>
+        </div>
+        {/* <div><h2 className='poppins text-lg ml-4 font-medium'>Search Project</h2></div>
         <div className='searchbar py-3 px-4'>
 
             <div className="bglightblue px-3 py-2 rounded-md flex items-center">
@@ -81,17 +70,17 @@ const Home = () => {
         <div className='flex flex-row  mx-5 items-center    mb-2 border-gray-200 py-2 border-b'>
           <img src={Recents} alt="profile" className='mr-2 h-5 w-5'/>
           <h3 className='poppins font-light text-black text-md'>Recents</h3>
-        </div>
+        </div> */}
         
-        <h2 className='font-medium text-black   text-md px-4'>FILTERS</h2>
+        <h2 className='font-medium text-gray-400  text-md px-4 md-2'>SEARCH BY</h2>
         <div className="flex flex-col space-y-4 px-4 py-2  pb-6 mb-2 border-gray-200">
-          <label htmlFor="projects" className="block text-sm text-black font-medium">LOCATION:</label>
+          <label htmlFor="projects" className="block text-sm text-gray-400 font-medium">LOCATION:</label>
           <Location/>
-          <label htmlFor="projects" className="block text-sm text-black font-medium">PROJECTS:</label>
+          <label htmlFor="projects" className="block text-sm text-gray-400 font-medium">PROJECTS:</label>
           <Projects/>
-          <label htmlFor="projects" className="block text-sm text-black font-medium">DEPARTMENTS:</label>
+          <label htmlFor="projects" className="block text-sm text-gray-400 font-medium">DEPARTMENTS:</label>
           <Departments/>
-          <label htmlFor="projects" className="block text-sm text-black font-medium">STATUS:</label>
+          <label htmlFor="projects" className="block text-sm text-gray-400 font-medium">STATUS:</label>
           <Status/>
         </div>
 
@@ -115,46 +104,164 @@ const Home = () => {
         
       </div> 
       <div className='flex  flex-col w-full  bglightblue  '>
+        <div class='flex flex-row justify-between items-center  bg-white px-5 py-2  '>
+            
+            
+            <div>
+                <input type='text' placeholder='Search Projects' className='border-none bg-gray-200 px-10 py-1'/>
+            </div>
+            
+            <div className='flex flex-row  px-5  items-center   mb-2 border-gray-200 '>
+            
+            <h2 className='poppins font-medium text-lg lightgreen mr-2'>Hi!, Karthik</h2>
+            <img src={Profile} alt="profile" className='h-7 w-7 mr-2'/>
+
+          </div>
+      </div>
         
           <div className='px-5 mt-5'>
-            <div class='flex flex-row justify-between items-center bg-white px-5 py-4 rounded-md shadow-md mb-5'>
-              <h2 class='font-semibold poppins text-3xl'>Projects</h2>
-              <h2 class='font-semibold poppins text-2xl bgdarkblue px-4 py-2 text-black rounded-lg'>Create New</h2>
+            <div class='flex flex-row justify-between items-center mb-5'>
+              {/* <div>
+                <input type='text' placeholder='Search Projects' className='border-none bg-gray-200 px-10 py-1'/>
+              </div> */}
+              <h2 className='lightgreen font-medium text-2xl'>Overview</h2>
+              <Link to="Newproject"  class='font-normal poppins text-xl green px-4 py-2 text-white rounded-lg'>Create New</Link>
             </div>
-            <div className='bgdarkblue px-4 py-2  rounded-md text-xl'>
+            {/* <div className='bgdarkblue px-4 py-2  rounded-md text-xl'>
               <Link to="Newproject" className=' text-white poppins font-semibold' >New Project</Link>
-            </div>
+            </div> */}
           </div>
-          <div className='projectslist'>
+          <div className='flex flex-row gap-5 justify-around mb-5 px-5'>
+              <div className='bg-white h-20 w-full rounded-md px-4 py-4'>
+                <h2 className='font-semibold text-xl text-gray-400'>Total Projects</h2>
+              </div>
+              <div className='bg-white h-20  w-full rounded-md px-4 py-4'>
+                <h2 className='font-semibold text-xl text-gray-400'>Projects Completed</h2>
+              </div>
+              <div className='bg-white h-20 w-full  rounded-md px-4 py-4'>
+                <h2 className='font-semibold text-xl text-gray-400'>Projects Pending</h2>
+              </div>
+
+            </div>
+          <div class="bg-white py-3 px-5 mx-5">
+            
+  <div class="overflow-x-auto">
+    <table class="min-w-full divide-y divide-gray-200">
+      <thead class="green text-white">
+        <tr>
+          <th class="py-2 px-4 text-left">Project Name</th>
+          <th class="py-2 px-4 text-left">Department</th>
+          <th class="py-2 px-4 text-left">Project Engineer</th>
+          <th class="py-2 px-4 text-left">Location</th>
+          <th class="py-2 px-4 text-left">End Date</th>
+        </tr>
+      </thead>
+      <tbody class="divide-y divide-gray-200">
+        <tr>
+          <td class="py-3 px-4 ">Metro</td>
+          <td class="py-3 px-4">Government</td>
+          <td class="py-3 px-4">Sameer</td>
+          <td class="py-3 px-4">Chennai</td>
+          <td class="py-3 px-4">11/12/24</td>
+        </tr>
+        <tr>
+          <td class="py-3 px-4">Metro</td>
+          <td class="py-3 px-4">Government</td>
+          <td class="py-3 px-4">Sameer</td>
+          <td class="py-3 px-4">Chennai</td>
+          <td class="py-3 px-4">11/12/24</td>
+        </tr>
+        <tr>
+          <td class="py-3 px-4 ">Metro</td>
+          <td class="py-3 px-4">Government</td>
+          <td class="py-3 px-4">Sameer</td>
+          <td class="py-3 px-4">Chennai</td>
+          <td class="py-3 px-4">11/12/24</td>
+        </tr>
+        <tr>
+          <td class="py-3 px-4">Metro</td>
+          <td class="py-3 px-4">Government</td>
+          <td class="py-3 px-4">Sameer</td>
+          <td class="py-3 px-4">Chennai</td>
+          <td class="py-3 px-4">11/12/24</td>
+        </tr>
+        <tr>
+          <td class="py-3 px-4 ">Metro</td>
+          <td class="py-3 px-4">Government</td>
+          <td class="py-3 px-4">Sameer</td>
+          <td class="py-3 px-4">Chennai</td>
+          <td class="py-3 px-4">11/12/24</td>
+        </tr>
+        <tr>
+          <td class="py-3 px-4">Metro</td>
+          <td class="py-3 px-4">Government</td>
+          <td class="py-3 px-4">Sameer</td>
+          <td class="py-3 px-4">Chennai</td>
+          <td class="py-3 px-4">11/12/24</td>
+        </tr>
+        <tr>
+          <td class="py-3 px-4 ">Metro</td>
+          <td class="py-3 px-4">Government</td>
+          <td class="py-3 px-4">Sameer</td>
+          <td class="py-3 px-4">Chennai</td>
+          <td class="py-3 px-4">11/12/24</td>
+        </tr>
+        <tr>
+          <td class="py-3 px-4">Metro</td>
+          <td class="py-3 px-4">Government</td>
+          <td class="py-3 px-4">Sameer</td>
+          <td class="py-3 px-4">Chennai</td>
+          <td class="py-3 px-4">11/12/24</td>
+        </tr>
+        <tr>
+          <td class="py-2 px-4 ">Metro</td>
+          <td class="py-2 px-4">Government</td>
+          <td class="py-2 px-4">Sameer</td>
+          <td class="py-2 px-4">Chennai</td>
+          <td class="py-2 px-4">11/12/24</td>
+        </tr>
+        <tr>
+          <td class="py-2 px-4">Metro</td>
+          <td class="py-2 px-4">Government</td>
+          <td class="py-2 px-4">Sameer</td>
+          <td class="py-2 px-4">Chennai</td>
+          <td class="py-2 px-4">11/12/24</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
+
+          {/* <div className='projectslist px-5 '>
             {
               projects.map(project => (
-                <div className='task mb-6' key={project._id} >
+                <div className='task mb-6 shadow-md rounded-lg' key={project._id} >
                 
                 <div className='bgdarkblue w-full h-12 px-10 py-3'>
-                    <h2 className=' text-white poppins' onClick={()=>postname(project.projectName)}>{project.projectName}</h2>
+                    <h2 className=' text-black poppins text-md font-medium' onClick={()=>postname(project.projectName)}>{project.projectName}</h2>
                 </div>
                 <div className='flex flex-row bg-white w-full'>
-                  <div className='flex items-center justify-center h-74 w-56 '>
+                  {/* <div className='flex items-center justify-center h-74 w-56 '>
                     <img src={Metro} alt='ima' className='h-32 w-32'/>
-                  </div>
-                  <div className='flex flex-col poppins w-full px-10 py-8 font-semibold'>
+                  </div> 
+                  <div className='flex flex-col poppins w-full px-10 pb-4 font-normal'>
                       <h2 className='my-1'>Department: <span className='font-normal text-gray-700'> {project.department}</span></h2>
-                      <h2 className='my-1'>Project <span className='font-normal text-gray-700'> {project.projectName}</span></h2>
+                      <h2 className='my-1'>Project: <span className='font-normal text-gray-700'> {project.projectName}</span></h2>
                       <h2 className='my-1'>Project Engineer: <span className='font-normal text-gray-700'> {project.projectEngineer} </span></h2>
                       <h2 className='my-1'>Location: <span className='font-normal text-gray-700'> {project.location} </span></h2>
-                      <h2 className='my-1'>Estimated Time: <span className='font-normal text-gray-700'>{project.estimatedTime} </span></h2>
+                      {/* <h2 className='my-1'>Estimated Time: <span className='font-normal text-gray-700'>{project.estimatedTime} </span></h2>
                       <h2 className='my-1'>Estimated Budget: <span className='font-normal text-gray-700'> {project.completionStatus} </span></h2>
                       <h2 className='my-1'>Completion Status:</h2>
                       <div className='flex flex-row w-full  items-center'>
                         <div className='h-2 w-full bgdarkblue mr-4'></div>
                         <h2>{project.completionStatus}</h2>
-                      </div>
+                      </div> 
                   </div>
                 </div>
             </div>
           
           ))}
-          </div>
+          </div> */}
 
       </div>
     </div>
